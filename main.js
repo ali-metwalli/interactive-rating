702eb7db1444d1a2ed1a0cde5e8ab5f50e5ef301
Eslam <eslamg@blackstoneeit.com>
2022-03-26 13:38:13 +0200

    feat: style and add rating

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 let submit = document.getElementById('submitButton');
+let ratings = document.querySelectorAll(".star");
+ratings.forEach(rating => {
+    rating.addEventListener("click", () => {
+        const index = rating.dataset.index;
+        handleRating(index)
+    })
+});
+
+function handleRating(index) {
+    for (let i = 0; i < ratings.length; i++) {
+        ratings[i].classList.remove("active");
+        if (i <= index) {
+            ratings[i].classList.toggle("active")
+        }
+    }
+}
 
 function buttonColors() {
     submit.style.backgroundColor = 'white';
